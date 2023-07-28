587e81ac719ea0ea25ee69a22e10aeee51f26ce3
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-07-28 22:38:22 +0200

    Add files via upload

---
+++ javascripts/discourse/api-initializers/full-row-bulk-select.js
@@ -0,0 +1,47 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { findRawTemplate } from "discourse-common/lib/raw-templates";
+import { schedule } from "@ember/runloop";
+import { RUNTIME_OPTIONS } from "discourse-common/lib/raw-handlebars-helpers";
+import { htmlSafe } from "@ember/template";
+
+export default {
+  name: "full-row-bulk-select",
+  
+  initialize() {
+    withPluginApi("0.8.7", (api) => {
+      api.modifyClass("component:topic-list-item", {
+        pluginId: "full-row-bulk-select",
+
+        renderTopicListItem() {
+          const template = findRawTemplate("list/topic-list-item");
+          if (template) {
+            this.set(
+              "topicListItemContents",
+              htmlSafe(template(this, RUNTIME_OPTIONS))
+            );
+            schedule("afterRender", () => {
+              if (this.isDestroyed || this.isDestroying) {
+                return;
+              }
+              if (this.selected && this.selected.includes(this.topic)) {
+                this.element.querySelector("input.bulk-select").checked = true;
+                if (!this.site.mobileView) {
+                  this.element.querySelector(".bulk-select.topic-list-data label").classList.add("selected");
+                } else {
+                  this.element.querySelector(".topic-list-data .pull-left label").classList.add("selected");
+                }
+              }
+              if (this._shouldFocusLastVisited()) {
+                const title = this._titleElement();
+                if (title) {
+                  title.addEventListener("focus", this._onTitleFocus);
+                  title.addEventListener("blur", this._onTitleBlur);
+                }
+              }
+            });
+          }
+        },
+      });
+    });
+  },
+};
